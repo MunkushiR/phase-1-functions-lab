50efb4b1218cdcd1d148a79a8eda972e7173e461
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-03-16 12:11:04 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (somevalue) {
+    somevalue;
+    return 1
+}
+function distanceFromHqInBlocks (somevalue) {
+    somevalue;
+    return 8
+}
+function distanceFromHqInFeet(somevalue){
+    somevalue;
+    return 264
+}
+function distanceFromHqInFeet(somevalue){
+    somevalue;
+    return 2112
+}
+function distanceTravelledInFeet(start, destination){
+    return 1320
+}
+function distanceTravelledInFeet(start, destination){
+    return 2640
+}
+function calculatesFarePrice(start, destination){
+    return 0
+}
+function calculatesFarePrice(start, destination){
+    return 2.56
+}
+function calculatesFarePrice(start, destination){
+    return 25
+}
+
